bdfc0d9b00f4a22830ebda4b5a3ab73592dfc55e
Jason Carpenter <jtcarpenter@gmail.com>
2017-05-29 18:12:41 +0100

    upgrades react-router to v4

--- src/index.js
+++ src/index.js
@@ -4,17 +4,29 @@ import GridContainer from './containers/gridContainer/GridContainer.jsx';
 import PlayerContainer from './containers/playerContainer/PlayerContainer.jsx';
 import {Provider} from 'react-redux';
 import appStore from './store/appStore';
-import {Router, Route, browserHistory, hashHistory} from 'react-router';
+import {Route, BrowserRouter, HashRouter, Switch} from 'react-router-dom';
 import indexCss from './index.scss';
 import KeyHandler from './components/keyHandler/KeyHandler.jsx';
 
 ReactDOM.render(
     <Provider store={appStore}>
         <KeyHandler>
-            <Router history={hashHistory}>
-                <Route path="/" component={GridContainer}></Route>
-                <Route path="/player/(:id)" component={PlayerContainer}></Route>
-            </Router>
+            <HashRouter>
+                <Switch>
+                    <Route
+                        exact
+                        path="/"
+                        component={GridContainer}
+                    >
+                    </Route>
+                    <Route
+                        exact
+                        path="/player/(:id)"
+                        component={PlayerContainer}
+                    >
+                    </Route>
+                </Switch>
+            </HashRouter>
         </KeyHandler>
     </Provider>,
     document.getElementById('app')
